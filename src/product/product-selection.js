cd6086179ef4a065c0691323e851cc863e0ff59e
Ashish Gollamudy <ashish_gollamudy@comcast.com>
2019-11-12 10:36:49 -0500

    updated the device to pixel4
    changed the product to pixel 4

--- src/product/product-selection.js
+++ src/product/product-selection.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { ProductDevices } from './product-device';
-import Fade from 'react-reveal/Fade';
+import React from "react";
+import { ProductDevices } from "./product-device";
+import Fade from "react-reveal/Fade";
 
-export const ProductPick = ({products,selectDevice}) => {
-    return (
-        <div className="container fluid">
-            <div className="row">
-                <div className="col-md-4 offset-md-5">
-                    <Fade left cascade>
-                        <h3 id="Title">Choose your Pixel 3</h3>
-                    </Fade>
-                </div>
-            </div>
-            <div className="row">
-                <ProductDevices products={products} selectDevice={selectDevice} />
-            </div>
+export const ProductPick = ({ products, selectDevice }) => {
+  return (
+    <div className="container fluid">
+      <div className="row">
+        <div className="col-md-4 offset-md-5">
+          <Fade left cascade>
+            <h3 id="Title">Choose your Pixel 4</h3>
+          </Fade>
         </div>
-    )
-}
+      </div>
+      <div className="row">
+        <ProductDevices products={products} selectDevice={selectDevice} />
+      </div>
+    </div>
+  );
+};
